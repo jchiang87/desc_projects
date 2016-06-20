3c520654be06e817fcd1e71f13713989cada0027
Jim Chiang <jchiang@slac.stanford.edu>
2016-06-20 09:16:55 -0700

    plot all six bands

--- phosimTables/compare_lcs.py
+++ phosimTables/compare_lcs.py
@@ -1,3 +1,7 @@
+"""
+Compare light curves from Stack Level 2 analysis of PhoSim data to the
+input CatSim light curves.
+"""
 from __future__ import print_function
 import os
 import pandas as pd
@@ -8,62 +12,91 @@ import lsst.utils
 from desc.monitor import LightCurve, RefLightCurves
 plt.ion()
 
-# Get the SN-like objects from the Phosim db tables.
-SNs = PhosimObjects(dof_range=(0, 30), chisq_range=(1e3, 1e10))
+class LightCurveComparer(object):
+    """
+    Class to overlay MC and measured light curves for the LSST bands.
+    """
+    def __init__(self, nx=3, ny=2, figsize=(15, 10), twinkles_db_info=None):
+        """
+        Set up the light curve comparer class.
+        """
+        self.nx, self.ny = nx, ny
+        plt.rcParams['figure.figsize'] = figsize
+        self.fig = None
 
-# Set up the reference light curve server.
-idSequence = SNs.data['sourceId'].tolist()
-obs = pd.read_csv(os.path.join(lsst.utils.getPackageDir('monitor'),
-                               'data', 'SelectedKrakenVisits.csv'),
-                  index_col='obsHistID')[['expMJD', 'filter', 'fiveSigmaDepth']]
-bp_dict = sims_photUtils.BandpassDict.loadBandpassesFromFiles()[0]
-ref_lc_server = RefLightCurves(idSequence=idSequence,
-                               tableName='TwinkSN',
-                               bandPassDict=bp_dict,
-                               observations=obs)
+        if twinkles_db_info is None:
+            self.twinkles_db_info = dict()
+        else:
+            self.twinkles_db_info = twinkles_db_info
 
-def plot_band(source_id, i):
-    print('processing', i, sourceId)
-    if i % (nx*ny) == 0:
-        fig = plt.figure()
-    ref_lc = ref_lc_server.lightCurve(sourceId, band)
-    ax = fig.add_subplot(*(ny, nx, (i % (nx*ny))+1))
-    plt.errorbar(ref_lc['time'], ref_lc['flux']*1e9,
-                 yerr=(ref_lc['fluxerr']*1e9).tolist(),
-                 fmt=':.')
-    xaxis_range = plt.axis()[:2]
-    plt.annotate('sourceId: %s' % sourceId,
-                 (0.05, 0.9), xycoords='axes fraction', size='small',
-                 color='blue')
+        # Get the SN-like objects from the Phosim db tables.
+        self.SNs = PhosimObjects(dof_range=(0, 30), chisq_range=(1e3, 1e10))
 
-    # Find the objectId of the nearest coadd object and its separation
-    # from the Phosim object.
-    objectId, sep = SNs.find_nearest_coadd_object(sourceId)
-    lc = LightCurve()
-    lc.build_lightcurve_from_db(objid=objectId, port=53306)
-    df = lc.lightcurve.to_pandas()
+        # Set up the reference light curve server.
+        self.idSequence = self.SNs.data['sourceId'].tolist()
+        obs = pd.read_csv(os.path.join(lsst.utils.getPackageDir('monitor'),
+                                       'data', 'SelectedKrakenVisits.csv'),
+                          index_col='obsHistID')[['expMJD', 'filter',
+                                                  'fiveSigmaDepth']]
+        bp_dict = sims_photUtils.BandpassDict.loadBandpassesFromFiles()[0]
+        self.ref_lc_server = RefLightCurves(idSequence=self.idSequence,
+                                            tableName='TwinkSN',
+                                            bandPassDict=bp_dict,
+                                            observations=obs)
+    def plot_bands(self, sourceId):
+        "Plot all LSST bands."
+        for i, band in enumerate('ugrizy'):
+            self.plot_band(i, sourceId, band)
 
-    selection = df['bandpass'] == 'lsst%(band)s' % locals()
-    plt.errorbar(df[selection]['mjd'], df[selection]['flux'],
-                 yerr=df[selection]['flux_error'].tolist(),
-                 color='red', fmt='--.')
+    def plot_band(self, i, sourceId, band):
+        """
+        Plot light curves for aa single LSST band.
+        """
+        if i % (self.nx*self.ny) == 0 or self.fig is None:
+            self.fig = plt.figure()
+        ref_lc = self.ref_lc_server.lightCurve(sourceId, band)
+        self.fig.add_subplot(*(self.ny, self.nx, (i % (self.nx*self.ny)) + 1))
+        plt.errorbar(ref_lc['time'], ref_lc['flux']*1e9,
+                     yerr=(ref_lc['fluxerr']*1e9).tolist(),
+                     fmt=':.')
+        xaxis_range = plt.axis()[:2]
+        plt.annotate('sourceId: %s' % sourceId,
+                     (0.05, 0.9), xycoords='axes fraction', size='small',
+                     color='blue')
 
-    axis_range = list(plt.axis())
-#    # Fix the x-axis to the reference light curve abscissa range.
-    axis_range[:2] = xaxis_range
-    # Add some vertical space in plot for the object id annotation.
-    axis_range[-1] *= 1.2
-    plt.axis(axis_range)
-    if i in (6, 7, 8):
+        # Find the objectId of the nearest coadd object and its separation
+        # from the Phosim object.
+        objectId, sep = self.SNs.find_nearest_coadd_object(sourceId)
+        lc = LightCurve()
+        self.twinkles_db_info['objid'] = objectId
+        lc.build_lightcurve_from_db(**self.twinkles_db_info)
+        df = lc.lightcurve.to_pandas()
+
+        selection = df['bandpass'] == 'lsst%(band)s' % locals()
+        plt.errorbar(df[selection]['mjd'], df[selection]['flux'],
+                     yerr=df[selection]['flux_error'].tolist(),
+                     color='red', fmt='--.')
+
+        axis_range = list(plt.axis())
+        # Fix the x-axis to the reference light curve abscissa range.
+        axis_range[:2] = xaxis_range
+        # Add some vertical space in plot for the object id annotation.
+        axis_range[-1] *= 1.2
+        plt.axis(axis_range)
         plt.xlabel('MJD')
-    if i in (0, 3, 6):
-        plt.ylabel('%(band)s band flux (nmagy)' % locals())
-    plt.annotate('objectId: %s\noffset: %.2farcsec' % (objectId, sep.arcsec),
-                 (0.05, 0.85), xycoords='axes fraction', size='small',
-                 color='red', verticalalignment='top')
+        plt.ylabel('flux (nmagy)')
+        plt.title('%(band)s band' % locals())
+        plt.annotate('objectId: %s\noffset: %.2f arcsec'%(objectId, sep.arcsec),
+                     (0.05, 0.85), xycoords='axes fraction', size='small',
+                     color='red', verticalalignment='top')
 
+if __name__ == '__main__':
+    db_info = dict(database='jc_desc',
+                   host='ki-sr01.slac.stanford.edu',
+                   port=3307)
 
-nx, ny = 1, 2
-plt.rcParams['figure.figsize'] = 10, 10
-for band in 'ugrizy':
-    for i, sourceId in enumerate(idSequence[:10]):
+    lc_comparer = LightCurveComparer(twinkles_db_info=db_info)
+    for i, sourceId in enumerate(lc_comparer.idSequence[:10]):
+        print("processing", i, sourceId)
+        lc_comparer.plot_bands(sourceId)
+        plt.savefig('%i_lcs.png' % sourceId)
